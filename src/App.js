626f9e0fb0d3ae10b50996605cd1c8cc8c25f1cd
Benjo <ben.ohman@gokstadakademiet.no>
2023-03-27 00:05:23 +0200

    setting up board and colors part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import {useState} from 'react'
 
-function App() {
+const width = 8;
+const candyColors =[
+  "blue",
+  "green",
+  "yellow",
+  "red",
+  "purple",
+  "orange"
+]
+
+
+const App = () => {
+    const[currentColorArrangement,setCurrentColorArrangement] = useState([]);
+
+    const createBoard = () => {
+      const randomColorArrangement = [];
+      for(let i = 0;i< width*width; i++) {
+        const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
+        randomColorArrangement.push(randomColor);
+      }
+    setCurrentColorArrangement(randomColorArrangement);
+    }
+
+    createBoard()
+
+    console.log(currentColorArrangement);
 
   return (
     <div>
